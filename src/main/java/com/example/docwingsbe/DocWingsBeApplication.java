206964872f8485da8ce5c29846f6975df78efbd2
IceRinne <1024622242@qq.com>
2024-06-05 00:18:45 +0800

    Initial commit

---
+++ src/main/java/com/example/docwingsbe/DocWingsBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.docwingsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocWingsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocWingsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/docwingsbe/DocWingsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docwingsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocWingsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
